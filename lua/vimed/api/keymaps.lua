94db283e7856a3276e76f966c5fd48e2e4bb2aad
ej-shafran <ejshafran@hilma.tech>
2023-10-08 02:48:19 +0300

    Update keymaps

--- lua/vimed/api/keymaps.lua
+++ lua/vimed/api/keymaps.lua
@@ -1,5 +1,5 @@
----@alias VimedKeymaps table<"n"|"x"|"v"|"V", table<string, string|fun()>?>
----
+---@alias VimedKeymaps table<"n"|"x"|"v"|"V", table<string, boolean|{ [1]: string|function, [2]: string? }>>
+
 local commands = require("vimed.api.commands")
 
 local M = {}
@@ -7,33 +7,33 @@ local M = {}
 ---@type VimedKeymaps
 M.default_keymaps = {
 	n = {
-		D = commands.delete,
-		J = commands.goto_file,
-		M = commands.chmod,
-		R = commands.rename,
-		U = commands.unmark_all,
-		d = commands.flag_file_deletion,
-		m = commands.mark,
-		o = commands.toggle_sort,
-		q = commands.quit,
-		r = commands.redisplay,
-		t = commands.toggle_marks,
-		u = commands.unmark,
-		x = commands.flagged_delete,
-		["<CR>"] = commands.enter,
-		["-"] = commands.back,
-		["^"] = commands.back,
-		["."] = commands.toggle_hidden,
-		["+"] = commands.create_dir,
-		["!"] = commands.shell_command,
-		["X"] = commands.shell_command,
-		["&"] = commands.async_shell_command,
-		["("] = commands.toggle_hide_details,
+		D = { commands.delete, "Delete" },
+		J = { commands.goto_file, "Goto File" },
+		M = { commands.chmod, "Change Permissions" },
+		R = { commands.rename, "Rename" },
+		U = { commands.unmark_all, "Unmark All" },
+		d = { commands.flag_file_deletion, "Flag For Deletion" },
+		m = { commands.mark, "Mark" },
+		o = { commands.toggle_sort, "Toggle Sort Order" },
+		q = { commands.quit, "Quit" },
+		r = { commands.redisplay, "Redisplay" },
+		t = { commands.toggle_marks, "Toggle Marks" },
+		u = { commands.unmark, "Unmark" },
+		x = { commands.flagged_delete, "Delete Flagged Files" },
+		["<CR>"] = { commands.enter, "Edit File" },
+		["-"] = { commands.back, "Back" },
+		["^"] = { commands.back, "Back" },
+		["."] = { commands.toggle_hidden, "Toggle Hidden" },
+		["+"] = { commands.create_dir, "Create Directory" },
+		["!"] = { commands.shell_command, "Shell Command" },
+		["X"] = { commands.shell_command, "Shell Command" },
+		["&"] = { commands.async_shell_command, "Async Shell Command" },
+		["("] = { commands.toggle_hide_details, "Toggle Hide Details" },
 	},
 	x = {
-		d = commands.flag_file_deletion,
-		u = commands.unmark,
-		m = commands.mark,
+		d = { commands.flag_file_deletion, "Flag For Deletion" },
+		u = { commands.unmark, "Unmark" },
+		m = { commands.mark, "Mark" },
 	},
 }
 
@@ -41,8 +41,8 @@ M.default_keymaps = {
 function M.setup_keymaps(config_maps)
 	for mode, tbl in pairs(config_maps) do
 		for lhs, rhs in pairs(tbl) do
-			if rhs ~= nil then
-				vim.keymap.set(mode, lhs, rhs, { buffer = 0 })
+			if type(rhs) ~= "boolean" then
+				vim.keymap.set(mode, lhs, rhs[1], { buffer = 0, desc = "Vimed: " .. (rhs[2] or lhs) })
 			end
 		end
 	end
